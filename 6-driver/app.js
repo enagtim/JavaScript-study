89915aa92ed416c2c95e6f069b0e41533c52a8c2
Nikita Kaberov <enagtim@mail.ru>
2024-07-24 14:41:26 +0300

    Отредактировал задачу!

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -3,6 +3,6 @@ const age = 18;
 const isDrunk = false;
 
 
-const canDrive = (age >= 18) && !hasLicince &&  !isDrunk;
+const canDrive = age >= 18 && hasLicince &&  !isDrunk;
 
 console.log(`Водитель может управлять транспортным средством: ${canDrive ? 'Да': 'Нет'} `);
