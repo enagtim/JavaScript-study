d3542ae5e0e1af29536271814259c16129a3d9e0
Nikita Kaberov <enagtim@mail.ru>
2024-07-29 09:06:19 +0300

    Добавил копирование массива, теперь при вводе исходног массива будет исходный массив.

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,15 +1,16 @@
 const arr = [1, 40, -5, 10, 0];
 function sort (array) {
+    let newarray = array.slice();
     for(let i = 0; i < arr.length; i++) {
         for(let j = 0; j < arr.length - 1; j++ ) {
-            if(array[j] > array[j + 1]){
-               [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            if(newarray[j] > newarray[j + 1]){
+               [newarray[j], newarray[j + 1]] = [newarray[j + 1], newarray[j]];
             }
         }
     }
-    return array
+    return newarray;
 }
-console.log(arr);
 const sortedArray = sort(arr);
 console.log(sortedArray);
+console.log(arr);
 
